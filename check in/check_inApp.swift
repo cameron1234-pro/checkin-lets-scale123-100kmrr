80b1ec23b05f0704cc9d25aba1be452394faa40e
cameron m dorsey <cmdprowash@gmail.com>
2026-02-25 00:11:34 -0500

    Initial Commit

---
+++ check in/check_inApp.swift
@@ -0,0 +1,17 @@
+//
+//  check_inApp.swift
+//  check in
+//
+//  Created by cameron Dorsey  on 2/25/26.
+//
+
+import SwiftUI
+
+@main
+struct check_inApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
